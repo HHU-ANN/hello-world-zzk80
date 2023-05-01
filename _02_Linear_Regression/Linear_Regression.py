48a90e5ae5846b89e33e628dd381ba968d7ba5bb
zzk80 <131932421+zzk80@users.noreply.github.com>
2023-05-01 21:30:01 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -10,23 +10,27 @@ except ImportError as e:
 
 def ridge(data):
     x,y = read_data()
-    lambd = 1e-10
-    weight = np.dot(np.linala.inv(np.(np.dot(x.T,x)+np.dot(lambd,np.eye(6)))),np.dot(x.T,y))
+    lambd = 2e-5
+    weight = np.matmul(np.linala.inv(np.(np.matmul(x.T,x)+np.matmul(lambd,np.eye(6)))),np.matmul(x.T,y))
     return weight @ data
+    pass
     
 def lasso(data):
-    label = 1e-10
-    x,y = read_data()
-    weight = np.zeros([6,6])
-    y1 = np.dot(weight,x)
-    rate = 1e-5
-    for i in range(int(1e10)):
+    label = 2e-5
+    x,Y = read_data()
+    weight = np.ones([6,6])
+    y = np.dot(weight,x)
+    loss = (np.sum(y - Y)**2) / 6
+    rate = 1e-10
+    for i in range(int(1e9)):
         y = np.dot(weight, x)
-        loss = (np.sum(y1 - y) ** 2) / 6
+        loss = (np.sum(y - Y) ** 2) / 6
         if loss < label:
             break
-        weight = weight - np.dot((y1 - y),x.T) * rate
+        dw = np.dot((y - Y),x.T)
+        weight = weight - dw * rate
     return weight @ data
+    pass
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
